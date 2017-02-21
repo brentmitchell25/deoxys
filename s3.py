83467f70244fd8a49f25043991d9f01786f6f0dd
U-KOCH\MITCHE7B <MITCHE7B@KII-6FKHGB2.kochind.com>
2017-02-21 10:43:45 -0600

    Add regex and refactor s3

--- s3.py
+++ s3.py
@@ -1,11 +1,19 @@
 from troposphere.s3 import Bucket, Tags
+import re
+
+regex = re.compile('[^a-zA-Z]')
+
 
 def s3(item, template, defaults):
     if 'Buckets' in item:
         for bucket in item['Buckets']:
+            parameters = {
+                "BucketName": bucket['BucketName'],
+                "DeletionPolicy": bucket['DeletionPolicy'] if 'DeletionPolicy' in item else 'Retain'
+            }
+
             template.add_resource(Bucket(
-                bucket['BucketName'].replace("-", "") + 'bucket',
-                BucketName=bucket['BucketName'],
-                DeletionPolicy=bucket['DeletionPolicy'] if 'DeletionPolicy' in item else 'Retain'
+                regex.sub("", bucket['BucketName']) + 'bucket',
+                **dict((k, v) for k, v in parameters.iteritems() if v is not None)
             ))
     return template
