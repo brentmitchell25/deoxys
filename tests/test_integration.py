84a93fa6409195549ace7824c520d097b5adcda8
Brent Mitchell <brent.mitchell@kochind.com>
2017-08-07 09:26:46 -0500

    Add log message

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,10 +5,12 @@ from test_utilities import runTest
 dynamodbClient = boto3.resource('dynamodb')
 s3Client = boto3.client('s3')
 
-applicationName = "Soke"
+applicationName = "ASH-PRODUCT-dev"
 services = dynamodbClient.Table('Application').query(
     KeyConditionExpression=Key('ApplicationName').eq(applicationName)
 )
+
+print(services)
 
 retVal = runTest(services, runWIthDefaultConfig=True)
 if 'iam' in retVal:
