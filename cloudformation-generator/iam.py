d08dca26143fed44717b810a5cfb816ecc03fa18
Brent Mitchell <brent.mitchell@kochind.com>
2018-01-30 10:51:09 -0600

    Add InstanceProfile

--- cloudformation-generator/iam.py
+++ cloudformation-generator/iam.py
@@ -1,4 +1,4 @@
-from troposphere.iam import Role, User, ManagedPolicy, Policy as Pol
+from troposphere.iam import Role, User, ManagedPolicy, Policy as Pol, InstanceProfile
 from awacs.aws import Action, Allow, Policy, Statement, Condition, AWSPrincipal, Bool, Principal
 from troposphere import Ref, Join
 import re
@@ -8,19 +8,21 @@ roleImg = './AWS_Simple_Icons/Security Identity & Compliance/SecurityIdentityCom
 
 regex = re.compile('[^a-zA-Z0-9]')
 
+
 def principalArn(principal, G, roleId):
     if principal["Owner"] == "User":
         return AWSPrincipal([Join("", ["arn:aws:iam::", Ref("AWS::AccountId"), ":user/", user]) for user in
-                principal["Name"]])
+                             principal["Name"]])
     elif principal["Owner"] == "CanonicalUser":
         return AWSPrincipal([Join("", ["arn:aws:iam::", Ref("AWS::AccountId"), ":user/", user]) for user in
-                principal["Name"]])
+                             principal["Name"]])
     elif principal["Owner"] == "Account":
         return AWSPrincipal([Join("", ["arn:aws:iam::", accountId, ":root"]) for accountId in principal["Name"]])
     elif principal["Owner"] == "Role":
         roles = []
         for role in principal["Name"]:
-            roles.append(Join("", ["arn:aws:iam::", Ref("AWS::AccountId"), ":role/", role]))
+            roles.append(
+                Join("", ["arn:aws:iam::", Ref("AWS::AccountId"), ":role/", role]))
             G.add_edge(roleId, regex.sub("", role) + "Role")
         return AWSPrincipal(roles)
     elif principal["Owner"] == "Service":
@@ -36,8 +38,10 @@ def principalArn(principal, G, roleId):
 
 
 def resourceArn(resource):
-    region = Ref("AWS::Region") if "Region" not in resource else resource["Region"]
-    accountId = Ref("AWS::AccountId") if "AccountId" not in resource else resource["AccountId"]
+    region = Ref(
+        "AWS::Region") if "Region" not in resource else resource["Region"]
+    accountId = Ref(
+        "AWS::AccountId") if "AccountId" not in resource else resource["AccountId"]
     if resource['Service'] == "All":
         return "*"
     elif resource['Service'] == "s3":
@@ -50,7 +54,6 @@ def resourceArn(resource):
     else:
         return Join("", ["arn:aws:", resource["Service"], ":", region, ":", accountId, ":",
                          resource["Resource"]])
-
 
 
 def getActions(statement):
@@ -71,10 +74,21 @@ def getStatement(statement, G, roleId):
     }
     return Statement(**dict((k, v) for k, v in parameters.iteritems() if v is not None))
 
+
 def policy(statements, G, roleId):
     return Policy(
-        Statement=[getStatement(statement, G=G, roleId=roleId) for statement in statements["Statements"]]
+        Statement=[getStatement(statement, G=G, roleId=roleId)
+                   for statement in statements["Statements"]]
     )
+
+
+def instanceProfile(role, path=None, instanceProfileName=None):
+    parameters = {
+        "Path": path,
+        "Roles": Ref(role),
+        "InstanceProfileName": instanceProfileName
+    }
+    return InstanceProfile(**dict((k, v) for k, v in parameters.iteritems() if v is not None))
 
 
 def iam(item, G, defaults):
@@ -92,7 +106,7 @@ def iam(item, G, defaults):
                 "Policies": [
                     Pol(PolicyName=pol["Name"],
                         PolicyDocument=policy(pol, G=G, roleId=roleId)) for pol in role["Policies"]
-                    ],
+                ],
                 "RoleName": role["RoleName"]
             }
             roleResource = Role(
@@ -101,3 +115,13 @@ def iam(item, G, defaults):
             )
             utilities.mergeNode(G, id=roleId, resource=roleResource, image=roleImg,
                                 name=role["RoleName"])
+            if 'InstanceProfile' in role:
+                instanceProfileId = roleId + 'InstanceProfile'
+                if str(role['InstanceProfile']) == 'true':
+                    instanceProfileResource = instanceProfile(roleResource)
+                else:
+                    instanceProfileResource = instanceProfile(
+                        roleResource, role['InstanceProfile']['Path'], role['InstanceProfile']['InstanceProfileName'])
+                utilities.mergeNode(G, id=instanceProfileId, resource=instanceProfileResource, image=roleImg,
+                                    name=role["RoleName"] + 'InstanceProfile')
+                G.add_edge(instanceProfileId, roleId)
